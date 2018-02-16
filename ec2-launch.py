434beff0b45ce6091c61f261c341098abcb8687a
clri <clr2176@columbia.edu>
2018-02-15 21:35:45 -0500

    to run

--- ec2-launch.py
+++ ec2-launch.py
@@ -1,6 +1,8 @@
 import boto
 
 
+keyname = "MY_KEY_NAME" #set as global
+
 '''
 Create a connection. Specify the region where you want to
 setup ec2 along with your security credentials
@@ -31,8 +33,13 @@ response = ec2.authorize_security_group_ingress(
              #'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
         ])
 
-#@TODO: get key pair name
-response = ec2.create_key_pair(KeyName='KEY_PAIR_NAME')
+
+'''
+create ec2 key pair and store in a .pem file for SSH
+'''
+response = ec2.create_key_pair(KeyName=keyname)
+response.save("./keyname.pem")
+
 '''
 Launch your ec2 instance. This requires ami image id and instance type.
 Refer to the AWS documentation for details. You need to setup your
@@ -40,7 +47,7 @@ key-pair and security group before launching.
 '''
 response = conn.run_instances(
         'ami-11ca2d78', #generic linux
-        key_name = 'KEY_PAIR_NAME', #from ec2
+        key_name = keyname, #from ec2
         instance_type = 't2.micro',
         security_groups = [security_grp])
 print response
@@ -51,7 +58,13 @@ You need to parse the returned response to print the following:
  - external IP address
  - which region instance was created
  - instance ID
+'''
+iid = response['Instances'][0]['InstanceId']
+print "external IP address: " + response['Instances'][0]['PublicIpAddress']
+print "region: " + response['Instances'][0]['Placement']['AvailabilityZone']
+print "instance ID: " + iid
 
+'''
 Do remember to use the correct key-pair and the security group with the
 correct access settings to make sure you can SSH into these instances.
 
@@ -59,16 +72,19 @@ Validate that your instance was created successfully by SSHing into the instance
 Screenshot
 
 '''
+str = ''
+while (str != 'STOP' and str != 'STOP\n'):
+	str = raw_input('type STOP to stop')
 
 
 '''
 Stop instance
 '''
 
-conn.stop_instances(instance_ids=['instance-id'])
+conn.stop_instances(instance_ids=[iid'])
 
 
 '''
 Terminate instance
 '''
-conn.terminate_instances(instance_ids=['instance-id'])
+conn.terminate_instances(instance_ids=[iid])
